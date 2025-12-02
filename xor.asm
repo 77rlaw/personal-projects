088a469f9cf9cc3c1d6aff69a76a5c23c159731f
77rlaw <rlaw7@illinois.edu>
2025-12-01 19:42:23 -0500

    \xor.asm

--- xor.asm
+++ xor.asm
@@ -1,7 +1,7 @@
 AND R0, R0, #0
 ADD R0, R0, R1
-NOT R0 ; 
+NOT R0, R0 ; 
 AND R0, R0, R2; anding NOT R1 && R2 -> R0
-NOT R2 ; notting R2
+NOT R2, R2 ; notting R2
 AND R1, R1, R2; anding NOT R2 && R1 -> R1
 ADD R0, R0, R1
